3b61e04642a2f14f04caa49b35381a716d66fb01
Giwan Kim <giwankim@gmail.com>
2022-07-10 07:59:16 +0900

    feat: add nginx conf

---
+++ scripts/deploy.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+## 변수 설정
+EXECUTION_PATH=$(pwd)
+SHELL_SCRIPT_PATH=$(dirname $0)
+BRANCH=$1
+PROFILE=$2
+
+PROJECT_NAME=infra-subway-stable
+REPOSITORY=/home/ubuntu/nextstep/$PROJECT_NAME
+
+txtrst='\033[1;37m' # White
+txtred='\033[1;31m' # Red
+txtylw='\033[1;33m' # Yellow
+txtpur='\033[1;35m' # Purple
+txtgrn='\033[1;32m' # Green
+txtgra='\033[1;30m' # Gray
+
+## 조건 설정
+if [[ $# -ne 2 ]]; then
+  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e "${txtgrn}  << 스크립트 🧐 >>${txtrst}"
+  echo -e ""
+  echo -e "${txtgrn} $0 브랜치이름 ${txtred}{ prod | dev }"
+  echo -e "${txtylw}=======================================${txtrst}"
+  exit
+fi
+
+function pull() {
+  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e ">> Pull Request 🏃"
+  git pull origin $BRANCH
+  echo -e "${txtylw}=======================================${txtrst}"
+}
+
+function build() {
+  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e ">> Gradle clean build 🏃"
+  ./gradlew clean build
+  echo -e ""
+  echo -e ">> Build 파일 복사 🏃"
+  cp $REPOSITORY/build/libs/*.jar $REPOSITORY
+  echo -e "${txtylw}=======================================${txtrst}"
+}
+
+function kill() {
+  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e ">> 현재 구동중인 애플리케이션 pid 확인 🏃"
+  CURRENT_PID=$(pgrep -f subway*.jar)
+  echo -e "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+  if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+  else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+  fi
+  echo -e "${txtylw}=======================================${txtrst}"
+}
+
+function start() {
+  echo -e "${txtylw}=======================================${txtrst}"
+  echo -e ">> 새 애플리케이션 배포 🏃"
+  JARFILE=$(ls -tr $REPOSITORY/ | grep jar | tail -n 1)
+  echo -e ${JARFILE}
+  echo -e "-Dspring.profiles.active=${PROFILE}"
+  nohup java -jar -Dspring.profiles.active=$PROFILE $JARFILE 1>$REPOSITORY/logs/$PROFILE 2>&1 &
+  echo -e "${txtylw}=======================================${txtrst}"
+}
+
+function check_df() {
+  git fetch
+  master=$(git rev-parse $BRANCH)
+  remote=$(git rev-parse origin/$BRANCH)
+
+  if [[ $master == $remote ]]; then
+    echo -e "[$(date)] Nothing to do !!! 😫"
+    exit 0
+  else
+    echo -e "> 리모트 브랜치가 변동되었습니다."
+    echo -e "> 로컬 브랜치를 업데이트하고, 다시 배포하겠습니다."
+    pull
+    build
+    kill
+    start
+  fi
+}
+
+## 프로젝트 디렉토리로 이동
+cd $REPOSITORY
+check_df
--- scripts/landing/smoke.js
+++ scripts/landing/smoke.js
@@ -3,7 +3,7 @@ import { check, group, sleep, fail } from "k6";
 
 export let options = {
   vus: 1,
-  duration: "30m",
+  duration: "1m",
 
   thresholds: {
     http_req_duration: ["p(99)<150"], // 99% of requests must complete below 0.15s
